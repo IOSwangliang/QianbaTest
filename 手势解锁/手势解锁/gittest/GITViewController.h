fd0b52bc3f372685fe83cc1988f38dd99a7d3d31
bobo <tan_anxi@163.com>
2016-08-25 15:01:16 +0800

    ddd

---
+++ 手势解锁/手势解锁/gittest/GITViewController.h
@@ -0,0 +1,13 @@
+//
+//  GITViewController.h
+//  手势解锁
+//
+//  Created by Qianba on 16/8/25.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GITViewController : UIViewController
+
+@end
---
+++ 手势解锁/手势解锁/gittest/GITViewController.m
@@ -0,0 +1,37 @@
+//
+//  GITViewController.m
+//  手势解锁
+//
+//  Created by Qianba on 16/8/25.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import "GITViewController.h"
+
+@interface GITViewController ()
+
+@end
+
+@implementation GITViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
